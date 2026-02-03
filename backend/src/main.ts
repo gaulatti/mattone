47e3a73aa5636865c669d6a18a8a24564a01f57e
Javier Godoy <javier@oxozon.io>
2026-02-02 20:38:09 -0500

    feat: Last fixes for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 8080);
+  const port = configService.get<number>('PORT', 3000);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
